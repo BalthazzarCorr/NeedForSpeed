bcf612fadf8a4381fc48b2c289dbdd3ed789c240
BalthazzarCorr <peev.petar@gmail.com>
2017-07-16 00:10:28 +0300

    qwe

--- NeedForSpeed/Program.cs
+++ NeedForSpeed/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeedForSpeed.Core;
 
 class Program
 {
    static void Main(string[] args)
    {
-
+      Engine engine = new Engine();
+      engine.Run();
    }
 }
 
